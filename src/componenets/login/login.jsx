cd8d9d24519a606247afe2ee61143232a42d24aa
Maritana Sampu <maritanasampu@gmail.com>
2019-11-04 23:59:23 +0200

    Fix typo in code

--- src/componenets/login/login.jsx
+++ src/componenets/login/login.jsx
@@ -7,26 +7,28 @@ export class Login extends React.Component {
     }
 
     render() {
-        return <div className="base-container">
-            <div className="header">Login</div>
-            <div className="content">
-                <div className="image">
-                    <img src={loginImg}/>
+        return (
+            <div className="base-container">
+                <div className="header">Login</div>
+                <div className="content">
+                    <div className="image">
+                        <img src={loginImg}/>
+                    </div>
+                    <div className="form">
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" name="username" placeholder="username"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" placeholder="password"/>
+                        </div>
+                    </div>
                 </div>
-                <div className="form">
-                    <div className="form-group">
-                        <label htmlFor="username">Username</label>
-                        <input type="text" name="username" placeholder="username"/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" name="password" placeholder="password"/>
-                    </div>
+                <div className="footer">
+                    <button type="button" className="btn">Login</button>
                 </div>
             </div>
-            <div className="footer">
-                <button type="button" className="btn">Login</button>
-            </div>
-        </div>
+            )
     }
 }
