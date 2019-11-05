b7585f60d4b4cd55b31e20f3f50fd8fea5e84fa4
Maritana Sampu <maritanasampu@gmail.com>
2019-11-05 02:09:40 +0200

    Add Login and Register component rendering

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React from 'react';
 import './App.scss';
 import { Login, Register } from "./componenets/login/index.jsx"
 
-function App() {
-  return (
-    <div className="App">
-      <Login/>
-    </div>
-  );
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      isLoginActive : true,
+    };
+  }
+
+  render() {
+    const { isLoginActive } = this.state;
+    return(
+        <div className="app">
+          <div className="login">
+            <div className="container">
+                {isLoginActive && <Login containerRef={(ref) => this.current = ref}/>}
+                {!isLoginActive && <Register containerRef={(ref) => this.current = ref}/>}
+            </div>
+
+          </div>
+        </div>
+    )
+  }
 }
 
 export default App;
